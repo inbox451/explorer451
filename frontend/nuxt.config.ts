6f5ce1de769b8d0b302d7e0372dd0f35f0518383
Claudio Medina <claudio.medina@mercedes-benz.com>
2025-07-13 22:15:22 +0100

    feat: setup frontend base with Nuxt + Tailwind + Shadcn

---
+++ frontend/nuxt.config.ts
@@ -0,0 +1,25 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from '@tailwindcss/vite'
+
+export default defineNuxtConfig({
+  compatibilityDate: '2025-05-15',
+  devtools: { enabled: true },
+  modules: ['@nuxt/eslint', '@nuxt/ui', 'shadcn-nuxt'],
+  css: ['~/assets/css/main.css'],
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: '',
+    /**
+     * Directory that the component lives in.
+     * @default "./components/ui"
+     */
+    componentDir: './components/ui'
+  }
+})
