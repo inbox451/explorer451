84b48b4ceae13826b3be996c7fddb03cae5c6414
Claudio Medina <claudio.medina@mercedes-benz.com>
2025-07-25 11:31:07 +0100

    feat: add default page title and suffix

--- frontend/app/app.vue
+++ frontend/app/app.vue
@@ -1,3 +1,10 @@
+<script setup lang="ts">
+useHead({
+  titleTemplate: (titleChunk) => {
+    return titleChunk ? `${titleChunk} - Explorer451` : 'Explorer451';
+  }
+})
+</script>
 <template>
   <NuxtLayout>
     <NuxtPage />
